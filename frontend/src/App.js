b1ffbe9ba6e2e5780f41d2915313824b81c6c8b8
Prithvi-Prabhu <149374826+Prithvi-Prabhu@users.noreply.github.com>
2023-12-31 15:39:12 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AccessLogQuery from './AccessLogQuery';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <AccessLogQuery />
+      </header>
+    </div>
+  );
+}
+
+export default App;
